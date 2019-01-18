7b5dae740f09c552d292dcf2f91b374a295dda9f
Parashara <parashara@gmail.com>
2019-01-17 16:01:25 -0800

    changed port to herokus

--- server.js
+++ server.js
@@ -17,7 +17,7 @@ mongoose.connect(MONGODB_URI);
 // Require all models
 var db = require("./models");
 
-var PORT = 3000;
+var PORT = process.env.port || 3000;
 
 // Initialize Express
 var app = express();
