dc0eeead8d7eb9dba30b332574f5c02832e83904
Parashara <parashara@gmail.com>
2019-01-20 21:26:39 -0800

    changed readme

--- README.md
+++ README.md
@@ -1,4 +1,12 @@
-# Scraper
-Scraper app using mongoose.
+# Indie Game Scraper
+Scraper app using mongoose. Scrape the latest news about indie games and leave notes on the site the you and others can see.
 
-To do a new scrape, you must go to "/scrape" in the end of the url. It deletes everything in the database and makes a list of new articles.
+# Motivation
+This was an app made to practice mongoose and scraping with cheerio.
+
+# Technologies Used
+Handlebars, mongoose, cheerio, express.
+
+# Code example
+
+![conditional code](/images/code.png?raw=true "Main condition")
--- server.js
+++ server.js
@@ -50,15 +50,9 @@ app.get("/", (req, res) => {
 app.get("/scrape", function (req, res) {
   // First, we grab the body of the html with axios
   axios.get("http://ind13.com").then(function (response) {
-    // Then, we load that into cheerio and save it to $ for a shorthand selector
     var $ = cheerio.load(response.data);
-
-    // Now, we grab every h2 within an article tag, and do the following:
-    $("div.cb-meta").each(function (i, element) {
-      // Save an empty result object
+    $("div.cb-meta").each(function () {
       var result = {};
-
-      // Add the text and href of every link, and save them as properties of the result object
       result.title = $(this)
         .find("a")
         .text();
